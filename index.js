c4fdc84508adf6d59ecf964ef0dd47506d8361a8
unknown <dangloiit5@gmail.com>
2024-11-16 22:39:34 +0700

    inforshop/admin

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const multer = require('multer');
 var bodyParser = require('body-parser')
 const { userInfo } = require('os')
 const { name } = require('ejs')
-
 
 const port = 3000
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
@@ -14,6 +14,16 @@ app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({    
   extended: true
 })); 
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './assets/upload/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+const upload = multer({ storage: storage }); 
 const dashboardRouter = require('./router/dashboard.js')
 const adminRouter = require('./router/admin.js')
 app.use('/', dashboardRouter)
