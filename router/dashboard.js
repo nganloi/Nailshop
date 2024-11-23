2ca865e4318821a7e9eb1ad92d312748a181c781
myngan1910 <nganttm.21it@vku.udn.vn>
2024-11-23 22:31:22 +0700

    information user

--- router/dashboard.js
+++ router/dashboard.js
@@ -12,17 +12,41 @@ const blog = require('./dashboard/blog.js')
 const cart = require('./dashboard/cart.js')
 const checkout = require('./dashboard/checkout.js')
 const contact = require('./dashboard/contact.js')
+const login = require('./dashboard/login.js')
+
 
 
 router.use('/', shop)
+//PRODUCT
 router.use('/product', products)
+
+//SINGLE-PRODUCT
 router.use('/single-product', productDetail)
+
+//SERVICE
 router.use('/service', service)
+
+//SERVICE-DETAIL
 router.use('/service-Detail', serviceDetail)
+
+//ABOUT
 router.use('/about', about)
+
+//BLOG
 router.use('/blog', blog)
+
+//BLOG-DETAIL
 router.use('/blog-Detail', blogDetail)
+
+//CART
 router.use('/cart', cart)
+
+//CHECKOUT
 router.use('/checkout', checkout)
+
+//CONTACT
 router.use('/contact', contact)
+
+//LOGIN
+router.use('/login', login)
 module.exports = router;
