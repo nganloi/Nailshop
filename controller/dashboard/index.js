2da7c89a2e367bb891f9c603f66937b74c56263e
unknown <dangloiit5@gmail.com>
2024-11-30 21:26:08 +0700

    loi/insert

--- controller/dashboard/index.js
+++ controller/dashboard/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-
+const marketing_shop= require('../../model/marketing-shop/view')
+const services = require('../../model/service/view')
 module.exports = {
     getShop: async(req,res) => {
-        res.render('./dashboard/index')
+        const mrt_shop= await marketing_shop.marketingShop()
+        const service = await services.Service()
+        res.render('./dashboard/index',{mrt_shop:mrt_shop,service:service})
     }
 }
