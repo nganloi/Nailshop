629338164317b88d6f71cd8cd06937c5a952d4e2
unknown <dangloiit5@gmail.com>
2024-12-03 20:15:42 +0700

    loi/view_Information

--- controller/dashboard/index.js
+++ controller/dashboard/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const marketing_shop= require('../../model/marketing-shop/view')
 const services = require('../../model/service/view')
+const category = require('../../model/category/view')
 module.exports = {
     getShop: async(req,res) => {
         const mrt_shop= await marketing_shop.marketingShop()
         const service = await services.Service()
-        res.render('./dashboard/index',{mrt_shop:mrt_shop,service:service})
+        const categorys = await category.category()
+        res.render('./dashboard/index',{mrt_shop:mrt_shop,service:service,category:categorys})
     }
 }
