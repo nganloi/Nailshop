ff96bd14e9ad7bb2a6893b9c7d8fd19588ea0eb4
myngan1910 <nganttm.21it@vku.udn.vn>
2024-12-03 21:18:39 +0700

    products

--- router/dashboard/products.js
+++ router/dashboard/products.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router();
  const productsController = require('../../controller/dashboard/products.js')
 
-
+ router.get('/:ID', productsController.getSignProducts)
  router.get('/', productsController.getProducts)
  module.exports = router;
